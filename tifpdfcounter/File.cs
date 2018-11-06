55f69f5ffdfadf939591c82dc3c8d8e7174d28bd
Ben Smith <bensmith720@gmail.com>
2018-11-05 23:02:28 -0500

    remove unnecessary nuget packages

--- tifpdfcounter/File.cs
+++ tifpdfcounter/File.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
-using BitMiracle.LibTiff.Classic;
 using System.Security.Cryptography;
 
 namespace TIFPDFCounter
