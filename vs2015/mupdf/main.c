90ca798604f30f2db35b7839bacee95ffc0ef227
Ben <bensmith720@gmail.com>
2019-03-19 23:48:51 -0400

    keep a single process alive to analyze files consecutively instead of starting/stopping a process for each file

--- vs2015/mupdf/main.c
+++ vs2015/mupdf/main.c
@@ -1,63 +1,174 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "mupdf\fitz.h"
 
-int main(int argc, char **argv)
+#define OK 0
+#define NO_INPUT 1
+#define INPUT_TOO_LONG 2
+#define CTX_FAILED 3
+#define HANDLERS_FAILED 4
+#define DOC_FAILED 5
+#define PAGE_FAILED 6
+
+static int get_line(char* buff, size_t sz)
 {
-	char *filename = argv[1];
-	float color_threshold = argc >= 3 ? atof(argv[2]) : -1;
-	int test_pixels = argc >= 4 ? atoi(argv[3]) : 1;
-	int test_color = color_threshold < 0 ? 0 : 1;
-	int test_options = test_pixels ? FZ_TEST_OPT_IMAGES | FZ_TEST_OPT_SHADINGS : 0;
+	int ch, extra;
 
-	fz_context *ctx = fz_new_context(NULL, NULL, FZ_STORE_DEFAULT);
-	fz_register_document_handlers(ctx);
-	fz_document *doc = fz_open_document(ctx, filename);
+	if (fgets(buff, sz, stdin) == NULL)
+		return NO_INPUT;
 
-	int pagecount = fz_count_pages(ctx, doc);
-	if (pagecount == 0)
-	{
-		fprintf(stderr, "Could not read page count");
-		exit(-1);
+	if (buff[strlen(buff) - 1] != '\n') {
+		extra = 0;
+		while (((ch = getchar()) != '\n') && (ch != EOF))
+			extra = 1;
+		return (extra == 1) ? INPUT_TOO_LONG : OK;
 	}
 
-	fprintf(stdout, "PageCount=%i\n", pagecount);
-	fflush(stdout);
+	buff[strlen(buff) - 1] = '\0';
+	return OK;
+}
 
-	for (int i = 0; i < pagecount; i++)
+static void do_color_test(fz_context* ctx, fz_page* page, int test_pixels, int color_threshold, int* is_color)
+{
+	fz_device* dev = NULL;
+	int test_options = 0;	
+
+	if (test_pixels)
+		test_options = FZ_TEST_OPT_IMAGES | FZ_TEST_OPT_SHADINGS;
+	else
+		test_options = 0;
+
+	fz_var(dev);
+	fz_try(ctx)
 	{
-		fz_device *dev;
-		fz_page *page;
-		fz_rect bounds;
-		int is_color = -1;
+		dev = fz_new_test_device(ctx, is_color, color_threshold, test_options, NULL);
+		fz_enable_device_hints(ctx, dev, FZ_NO_CACHE | FZ_DONT_INTERPOLATE_IMAGES);
+		fz_run_page(ctx, page, dev, &fz_identity, NULL);
+	}
+	fz_always(ctx)
+	{
+		fz_drop_device(ctx, dev);
+	}
+	fz_catch(ctx)
+		fz_rethrow(ctx);
+}
 
-		page = fz_load_page(ctx, doc, i);
-		fz_bound_page(ctx, page, &bounds);
+static void process_page(fz_context* ctx, fz_document* doc, int number, int test_pixels, int color_threshold)
+{
+	fz_page* page = NULL;
+	fz_rect mediabox;
+	float width = 0;
+	float height = 0;
+	int is_color = -1;
+	int test_color = 1;
+
+	if (color_threshold < 0)
+		test_color = 0;
+	else
+		test_color = 1;
+
+	fz_var(page);
+	fz_try(ctx)
+	{
+		page = fz_load_page(ctx, doc, number);
+		fz_bound_page(ctx, page, &mediabox);
+		width = mediabox.x1 - mediabox.x0;
+		height = mediabox.y1 - mediabox.y0;
 
 		if (test_color)
-		{
-			fz_try(ctx)
-			{
-				dev = fz_new_test_device(ctx, &is_color, color_threshold, test_options, NULL);
-				fz_enable_device_hints(ctx, dev, FZ_NO_CACHE | FZ_DONT_INTERPOLATE_IMAGES);
-				fz_run_page(ctx, page, dev, &fz_identity, NULL);
-			}
-			fz_always(ctx)
-			{
-				fz_drop_device(ctx, dev);
-			}
-			fz_catch(ctx)
-			{
-				is_color = -1;
-			}
-		}
-
-		fz_drop_page(ctx, page);
-		fprintf(stdout, "Page=%i Size=%f,%f Color=%i\n", (i + 1), (bounds.x1 - bounds.x0), (bounds.y1 - bounds.y0), is_color);
+			do_color_test(ctx, page, test_pixels, color_threshold, &is_color);
+		
+		fprintf(stdout, "Page=%d Width=%f Height=%f Color=%d\n", number + 1, width, height, is_color);
 		fflush(stdout);
 	}
-	fz_drop_document(ctx, doc);
+	fz_always(ctx)
+	{
+		fz_drop_page(ctx, page);
+	}
+	fz_catch(ctx)
+		fz_rethrow(ctx);
+}
+
+static int process_file(char* filename, int test_pixels, int color_threshold)
+{
+	fz_context* ctx;
+	fz_document* doc = NULL;
+	int page_count;
+
+	ctx = fz_new_context(NULL, NULL, FZ_STORE_DEFAULT);
+	if (!ctx)
+	{
+		fprintf(stderr, "Failed to create context\n");
+		return CTX_FAILED;
+	}
+	
+	fz_try(ctx)
+	{
+		fz_register_document_handlers(ctx);
+	}
+	fz_catch(ctx)
+	{
+		fprintf(stderr, "Cannot initialize mupdf: %s\n", fz_caught_message(ctx));
+		fz_drop_context(ctx);
+		return HANDLERS_FAILED;
+	}
+
+	fz_var(doc);
+	fz_try(ctx)
+	{
+		doc = fz_open_document(ctx, filename);
+		page_count = fz_count_pages(ctx, doc);
+		fprintf(stdout, "PageCount=%d\n", page_count);
+		fflush(stdout);
+		for (int i = 0; i < page_count; i++)
+		{
+			process_page(ctx, doc, i, test_pixels, color_threshold);
+		}
+		fz_drop_document(ctx, doc);
+	}
+	fz_catch(ctx)
+	{
+		fprintf(stderr, "Failed to open document: %s\n", fz_caught_message(ctx));
+		fz_drop_document(ctx, doc);
+		fz_drop_context(ctx);
+		return DOC_FAILED;
+	}
+
 	fz_drop_context(ctx);
+	return OK;
+}
+
+int main(int argc, char** argv)
+{
+	while (1)
+	{
+		char buff[1024];
+		char filename[5] = "";
+		int test_pixels, rc;
+		float color_threshold = 0;
+
+		rc = get_line(buff, sizeof(buff));
+		if (rc == NO_INPUT)
+		{
+			fprintf(stderr, "No input\n");
+		}
+		else if (rc == INPUT_TOO_LONG)
+		{
+			fprintf(stderr, "Input too long [%s]\n", buff);
+		}
+		else if ((rc = sscanf(buff, " \"%[^\"]\" %d %f", filename, &test_pixels, &color_threshold)) != 3)
+		{
+			fprintf(stderr, "Failed to parse input [%s]\n", buff);
+		}
+		else
+		{
+			fprintf(stdout, "rc=%d filename=%s test_pixels=%d color_threshold=%f\n", rc, filename, test_pixels, color_threshold);
+			rc = process_file(filename, test_pixels, color_threshold);
+		}
+
+		fprintf(stdout, "RC=%d\n", rc);
+	}
 
 	return 0;
 }
