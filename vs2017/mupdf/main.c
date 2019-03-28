cb00ebc1196488c8141bbfa1084bd54332ea424f
Ben <bensmith720@gmail.com>
2019-03-27 23:02:58 -0400

    testing FreeImage

--- vs2017/mupdf/main.c
+++ vs2017/mupdf/main.c
@@ -1,8 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mupdf\fitz.h"
+#include "FreeImage.h"
 
-int main(int argc, char **argv)
+
+int main(int argc, char** argv)
+{
+	const char* filename = "C:\\temp\\bw-scan-wf.tif";
+	FreeImage_Initialise(TRUE);
+	FREE_IMAGE_FORMAT fif = FIF_UNKNOWN;
+	fif = FreeImage_GetFileType(filename, 0);
+	if (fif == FIF_UNKNOWN)
+	{
+		printf("Could not detect file format\n");
+	}
+	else if (fif == FIF_TIFF || fif == FIF_ICO || fif == FIF_GIF)
+	{
+		//Multi-page type of file
+		printf("Opening multi-page file: %s\n", filename);
+		FIMULTIBITMAP* dib = FreeImage_OpenMultiBitmap(fif, filename, FALSE, TRUE, FALSE, 0);
+		int pageCount = FreeImage_GetPageCount(dib);
+		printf("PageCount=%d\n", pageCount);
+
+		for (int i = 0; i < pageCount; i++)
+		{
+			FIBITMAP* page = FreeImage_LockPage(dib, i);
+			int width, height, bpp;
+			float xres, yres;
+			width = FreeImage_GetWidth(page);
+			height = FreeImage_GetHeight(page);
+			bpp = FreeImage_GetBPP(page);
+			xres = FreeImage_GetDotsPerMeterX(page);
+			yres = FreeImage_GetDotsPerMeterY(page);
+
+			printf("Page=%d Size=%dx%d BPP=%d DPI=%fx%f\n", i + 1, width, height, bpp, xres, yres);
+			FreeImage_UnlockPage(dib, page, FALSE);
+		}
+
+		FreeImage_CloseMultiBitmap(dib, 0);
+
+	}
+	else
+	{
+		//Single-page type of file
+		fprintf("Opening single-page file: %s\n", filename);
+		FIBITMAP* dib = FreeImage_Load(fif, filename, 0);
+		if (dib == NULL)
+		{
+			printf("Failed to open image\n");
+		}
+		else
+
+		{
+
+		}
+	}
+	FreeImage_DeInitialise();
+	printf("Program finished\n");
+	getc(stdin);
+}
+
+int main2(int argc, char **argv)
 {
 	char *filename = argv[1];
 	float color_threshold = argc >= 3 ? atof(argv[2]) : -1;
